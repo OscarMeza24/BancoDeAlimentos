e0c717853503cc7ea5ed340122bbff7adfae0b9b
OscarMeza <weynomames53@gmail.com>
2025-10-06 20:41:01 -0500

    Asegurar que la ubicación de recogida se establezca correctamente al usar el operador de aserción no nulo

--- app/alimentos/nuevo/page.tsx
+++ app/alimentos/nuevo/page.tsx
@@ -64,7 +64,7 @@ export default function NuevoAlimentoPage() {
       if (profileData?.address) {
         setFormData((prev) => ({
           ...prev,
-          pickup_location: profileData.address,
+          pickup_location: profileData.address!,
         }))
       }
     } catch (error) {
